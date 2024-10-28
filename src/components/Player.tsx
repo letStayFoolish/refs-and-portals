b213077babaa0e7f87c25d2b0f6bbe6779b8d1e2
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-10-28 20:52:41 +0100

    introducing `useRef()`

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Player: React.FC = () => {
+  const [enteredPlayerName, setEnteredPlayerName] = useState<string | null>(
+    null,
+  );
+
+  const playerNameRef = useRef<HTMLInputElement>(null);
+
+  function handleOnClick() {
+    if (!playerNameRef || playerNameRef.current === null) return;
+
+    setEnteredPlayerName(playerNameRef.current.value);
+  }
+
+  // Instead of writing it like this: enteredPlayerName ? enteredPlayerName : "some text"
+  // We can do it in a short way: enteredPlayerName ?? "some text"
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameRef} type="text" />
+        <button onClick={handleOnClick}>Set Name</button>
       </p>
     </section>
   );
